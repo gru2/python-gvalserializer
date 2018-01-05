2fb8f08598708871cd7311f03dd0fe7cdd6fbd50
gru2 <Radovan.Obradovic@gmail.com>
2018-01-05 20:50:56 +0100

    Working on gvalserializer.

--- gvalserializer.py
+++ gvalserializer.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import struct
 
 
@@ -7,9 +7,17 @@ class FileBinaryStream:
     def __init__(self):
         self.file = None
 
+    def open(self, fileName, mode):
+        self.file = open(fileName, mode)
+
+    def close(self):
+        if self.file is not None:
+            self.file.close()
+            self.file = None
+
     def _read(self, fmt):
         sz = struct.calcsize(fmt)
-        return struct.unpack(fmt, self.file.read(sz))
+        return struct.unpack(fmt, self.file.read(sz))[0]
 
     def _write(self, fmt, x):
         s = struct.pack(fmt, x)
@@ -29,7 +37,7 @@ class FileBinaryStream:
 
     def readString(self):
         len = self.readInt()
-        return self.file.read(len)
+        return self.file.read(len).decode("ascii")
 
     def writeByte(self, x):
         self._write("<B", x)
@@ -45,7 +53,19 @@ class FileBinaryStream:
 
     def writeString(self, x):
         self.writeInt(len(x))
-        self.file.write(x)
+        self.file.write(x.encode("ascii"))
+
+
+GVT_NULL = 0
+GVT_BOOL = 1
+GVT_INT = 2
+GVT_LONG = 3
+GVT_FLOAT = 4
+GVT_DOUBLE = 5
+GVT_STRING = 6
+GVT_MULTI_ARRAY = 7
+GVT_MAP = 8
+GVT_GENERIC = 9
 
 
 class GValSerializer:
@@ -53,4 +73,136 @@ class GValSerializer:
     def __init__(self):
         self.binaryStream = None
 
+    def write(self, x):
+        if x is None:
+            self.binaryStream.writeInt(GVT_NULL)
+            return
+        if type(x) is bool:
+            self.binaryStream.writeInt(GVT_BOOL)
+            self.binaryStream.writeByte(int(x))
+            return
+        if type(x) is int:
+            self.binaryStream.writeInt(GVT_INT)
+            self.binaryStream.writeInt(x)
+            return
+        if type(x) is float:
+            self.binaryStream.writeInt(GVT_DOUBLE)
+            self.binaryStream.writeDouble(x)
+            return
+        if type(x) is str:
+            self.binaryStream.writeInt(GVT_STRING)
+            self.binaryStream.writeString(x)
+            return
+        if type(x) is list:
+            self.binaryStream.writeInt(GVT_MULTI_ARRAY)
+            self.binaryStream.writeInt(GVT_GENERIC)
+            self.binaryStream.writeInt(1)
+            self.binaryStream.writeInt(len(x))
+            for y in x:
+                self.write(y)
+            return
+        if type(x) is dict:
+            self.binaryStream.writeInt(GVT_MAP)
+            self.binaryStream.writeInt(len(x))
+            for k in x:
+                self.write(k)
+                self.write(x[k])
+            return
+        if type(x) == np.ndarray:
+            nDims = len(x.shape)
+            nElems = x.size
+            xt = x.reshape(nElems)
+            if x.dtype == np.float64:
+                self.binaryStream.writeInt(GVT_DOUBLE)
+                self.binaryStream.writeInt(nDims)
+                for dim in x.shape:
+                    self.binaryStream.writeInt(dim)
+                for i in range(nElems):
+                    self.binaryStream.writeDouble(xt[i])
+                return
+            if x.dtype == np.float32:
+                self.binaryStream.writeInt(GVT_FLOAT)
+                self.binaryStream.writeInt(nDims)
+                for dim in x.shape:
+                    self.binaryStream.writeInt(dim)
+                for i in range(nElems):
+                    self.binaryStream.writeFloat(xt[i])
+                return
+            if x.dtype == np.int32:
+                self.binaryStream.writeInt(GVT_INT)
+                self.binaryStream.writeInt(nDims)
+                for dim in x.shape:
+                    self.binaryStream.writeInt(dim)
+                for i in range(nElems):
+                    self.binaryStream.writeInt(xt[i])
+                return
+        self.error("uanble to wirte object")
 
+    def read(self):
+        t = self.binaryStream.readInt()
+        if t == GVT_NULL:
+            return None
+        if t == GVT_BOOL:
+            return bool(self.binaryStream.readByte())
+        if t == GVT_INT:
+            return self.binaryStream.readInt()
+        if t == GVT_FLOAT:
+            return self.binaryStream.readFloat()
+        if t == GVT_DOUBLE:
+            return self.binaryStream.readDouble()
+        if t == GVT_STRING:
+            return self.binaryStream.readString()
+        if t == GVT_MULTI_ARRAY:
+            print("GVT_MULTI_ARRAY")
+            et = self.binaryStream.readInt()
+            print("et = ", et)
+            nDims = self.binaryStream.readInt()
+            print("nDims = ", nDims)
+            if et == GVT_GENERIC:
+                if nDims != 1:
+                    self.error("unable to load multidimensional Generic.")
+                    return None
+                nElems = self.binaryStream.readInt()
+                print("nElems = ", nElems)
+                x = []
+                for i in range(nElems):
+                    x.append(self.read())
+                print("x = ", x)
+                return x
+            shape = []
+            for i in range(nDims):
+                shape.append(self.binaryStream.readInt())
+            nElems = 1
+            for i in shape:
+                nElems *= i
+            if et == GVT_DOUBLE:
+                x = np.zeros(nElems, np.float64)
+                for i in range(nElems):
+                    x[i] = self.binaryStream.readDouble()
+                return x.reshape(shape)
+            if et == GVT_FLOAT:
+                x = np.zeros(nElems, np.float32)
+                for i in range(nElems):
+                    x[i] = self.binaryStream.readFloat()
+                return x.reshape(shape)
+            if et == GVT_INT:
+                x = np.zeros(nElems, np.int32)
+                for i in range(nElems):
+                    x[i] = self.binaryStream.readInt()
+                return x.reshape(shape)
+            self.error("unable to read multiarray")
+            return None
+        if t == GVT_MAP:
+            nElems = self.binaryStream.readInt()
+            x = {}
+            for i in range(nElems):
+                k = self.read()
+                v = self.read()
+                x[k] = v
+            return x
+        self.error("uanble to read object")
+        return None
+
+    def error(self, msg):
+        print("error:", msg)
+        exit(1)
---
+++ tests.py
@@ -0,0 +1,67 @@
+import gvalserializer
+import unittest
+import numpy as np
+
+class TestFileBinaryStream(unittest.TestCase):
+
+    def testOpenClose(self):
+        s = gvalserializer.FileBinaryStream()
+        s.open("pera.bin", "wb")
+        s.close()
+        s.open("pera.bin", "rb")
+        s.close()
+
+    def testWriteRead(self):
+        s = gvalserializer.FileBinaryStream()
+        s.open("pera.bin", "wb")
+        s.writeByte(255)
+        s.writeInt(-1)
+        s.writeFloat(1.5)
+        s.writeDouble(-2.25)
+        s.writeString("foobar")
+        s.close()
+        s.open("pera.bin", "rb")
+        self.assertEqual(s.readByte(), 255)
+        self.assertEqual(s.readInt(), -1)
+        self.assertEqual(s.readFloat(), 1.5)
+        self.assertEqual(s.readDouble(), -2.25)
+        self.assertEqual(s.readString(), "foobar")
+        s.close()
+
+
+class TestGValSerializer(unittest.TestCase):
+
+    def testGValSerializer(self):
+        fbs = gvalserializer.FileBinaryStream()
+        fbs.open("pera.bin", "wb")
+        gvs = gvalserializer.GValSerializer()
+        gvs.binaryStream = fbs
+        gvs.write(None)
+        gvs.write(True)
+        gvs.write(False)
+        gvs.write(34)
+        gvs.write(23.25)
+        gvs.write("foobar")
+        gvs.write([100, 301, "Pera"])
+        gvs.write({"foo": 4, "bar": "baz"})
+        gvs.write(np.array([1.0, 2.5, -4.25]))
+        gvs.write(np.array([[1.0, 2.5, 1.5], [2.0, 3.0, -5.5]], dtype=np.float32))
+        fbs.close()
+        fbs.open("pera.bin", "rb")
+        self.assertEqual(gvs.read(), None)
+        self.assertEqual(gvs.read(), True)
+        self.assertEqual(gvs.read(), False)
+        self.assertEqual(gvs.read(), 34)
+        self.assertEqual(gvs.read(), 23.25)
+        self.assertEqual(gvs.read(), "foobar")
+        self.assertEqual(gvs.read(), [100, 301, "Pera"])
+        self.assertEqual(gvs.read(), {"foo": 4, "bar": "baz"})
+        t = gvs.read()
+        print("t = ", t)
+        self.assertTrue(np.array_equal(t, np.array([1.0, 2.5, -4.25])))
+        self.assertTrue(np.array_equal(gvs.read(), np.array([[1.0, 2.5, 1.5], [2.0, 3.0, -5.5]], dtype=np.float32)))
+        fbs.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
